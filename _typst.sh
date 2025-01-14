fade31024354b2536040fa396e02c490eb5ad206
Tilman Stehr <tilman@tilman.ninja>
2025-01-14 22:22:34 +0100

    Wrap filename in typst if necessary

--- _typst.sh
+++ _typst.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
 
-cat << EOF | typst compile - $2
+input_file=$1
+output_file=$2
+
+cat << EOF | typst compile - $output_file
 #import "@preview/cmarker:0.1.1"
-#set page(footer: context [
-  \`$1\`
-  #h(1fr)
-  #if counter(page).final().at(0) > 1 [  
-    #counter(page).display(
-      "1/1",
-      both: true,
-    )
-  ]
-])
+
+#let filename = "$input_file"
+
+#let wrapAfterSymbol(body) = [
+  #show regex("[^a-zA-Z]"): it => it + sym.zws
+  #body
+]
+
+#set page(
+  footer: context [
+    #if counter(page).final().at(0) > 1 {
+      let pagenum = counter(page).display(
+        "1/1",
+        both: true,
+      )
+      let pagenumsize = measure(pagenum)
+      [
+        #box(
+          width: 100% - pagenumsize.width - 5pt,
+          baseline: 100% - pagenumsize.height,
+          wrapAfterSymbol(raw(filename)),
+        )
+        #h(1fr)
+        #pagenum
+      ]
+    } else {
+      wrapAfterSymbol(raw(filename))
+    }
+  ],
+)
+
 #cmarker.render(
-  read("$1"),
+  read(filename),
   blockquote: box.with(stroke: (left: 1pt + black), inset: (left: 5pt, y: 6pt)),
   scope: (image: (path, alt: none) => image(path, alt: alt)),
 )
