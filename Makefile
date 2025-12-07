d7c0e6bf69a8b6227c1548d57b594771e236d251
Tilman Stehr <tilman@tilman.ninja>
2025-12-07 15:46:48 +0100

    Add make target for flattened pdfs

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-.PHONY: all filenames images pdfs clean
+.PHONY: all filenames images pdfs pdfs-flattened clean
 
 MDS = $(shell fd -I -t f '.*\.md')
 PDFS := $(patsubst %.md,pdfs/%.pdf,$(MDS))
+PDFS_FLATTENED := $(patsubst %.md,pdfs-flattened/%.pdf,$(MDS))
 
 all: filenames images
 	$(MAKE) pdfs
+	$(MAKE) pdfs-flattened
 
 filenames: 
 	python _filenames.py
@@ -19,5 +21,15 @@ pdfs/%.pdf : %.md
 	@mkdir -p "$(@D)"
 	./_typst.sh $< $@
 
+pdfs-flattened:
+	$(MAKE) -j12 $(PDFS_FLATTENED)
+
+pdfs-flattened/%.pdf: %.md
+	@mkdir -p "$(@D)" .temp
+	@tmpfile=$$(mktemp -p .temp); \
+	trap 'rm -f "$$tmpfile"' EXIT; \
+	recipemd --flatten "$<" > "$$tmpfile"; \
+	./_typst.sh "$$tmpfile" "$@" "$<" true
+
 clean:
-	$(RM) -r pdfs
+	$(RM) -r pdfs pdfs-flattened
--- _typst.sh
+++ _typst.sh
@@ -2,11 +2,26 @@
 
 input_file=$1
 output_file=$2
+filename=${3:-$input_file}
+ignore_image_not_found=${4:-false}
 
 cat << EOF | typst compile - $output_file
 #import "@preview/cmarker:0.1.1"
+#import "@preview/based:0.2.0": base64
 
-#let filename = "$input_file"
+#let inputFile = "$input_file"
+#let filename = "$filename"
+#let ignoreImageNotFound=$ignore_image_not_found
+
+#let context-function = (context { }).func()
+#let unique-string(path) = base64.encode(path)
+#let maybe-image(path, alt: none) = context {
+  let not-found = []
+  let path-label = label(unique-string(path))
+  let first-time = query(context-function).len() == 0
+  let used-path = query(path-label).len() > 0
+  if first-time or used-path [#image(path, alt: alt)#path-label] else { not-found }
+}
 
 #let wrapAfterSymbol(body) = [
   #show regex("[^a-zA-Z]"): it => it + sym.zws
@@ -39,8 +54,12 @@ cat << EOF | typst compile - $output_file
 )
 
 #cmarker.render(
-  read(filename),
+  read(inputFile),
   blockquote: box.with(stroke: (left: 1pt + black), inset: (left: 5pt, y: 6pt)),
-  scope: (image: (path, alt: none) => image(path, alt: alt)),
+  scope: (image: (path, alt: none) => if ignoreImageNotFound {
+    maybe-image(path, alt: alt)
+  } else {
+    image(path, alt: alt)
+  }),
 )
 EOF
