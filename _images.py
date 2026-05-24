32b7406deab93de5f82dca18fd3c3bf909150fdc
Tilman Stehr <tilman@tilman.ninja>
2026-05-24 20:57:34 +0200

    Rewrite images script for recipemd 5.0.0

--- _images.py
+++ _images.py
@@ -1,21 +1,20 @@
 import glob
 import os
-import re
 import sys
+from typing import List
 import urllib.request
 from pprint import pprint
-from commonmark import Parser
-from commonmark.node import NodeWalker
+from markdown_it import MarkdownIt
+from markdown_it.token import Token
 from yarl import URL
 
-from recipemd._vendor.commonmark_extensions.plaintext import CommonMarkToCommonMarkRenderer
 from recipemd.data import RecipeParser, RecipeSerializer
-from unidecode import unidecode
+from mdformat.renderer import MDRenderer
 
 root_path = os.path.realpath('.')
 
-commonmark_parser = Parser()
-commonmark_renderer = CommonMarkToCommonMarkRenderer()
+markdown_it = MarkdownIt()
+renderer = MDRenderer()
 
 recipe_parser = RecipeParser()
 recipe_serializer = RecipeSerializer()
@@ -26,31 +25,52 @@ def urlopen_user_agent(url: str):
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
     })
     return urllib.request.urlopen(request)
- 
-def download_recipe_images(recipe_path):
-    with open(recipe_path, 'r', encoding='UTF-8') as file:
-        ast = commonmark_parser.parse(file.read())
 
+def transform_image(token: Token, recipe_path: str):
+    src = token.attrGet('src')
+
+    if not isinstance(src, str):
+        return False
+
+    url = URL(src)
+    if not url.is_absolute():
+        return False
+
+    with urlopen_user_agent(str(url)) as resp:
+        image = resp.read()
+
+    image_path = os.path.join(root_path, 'images',  url.name)
+    with open(image_path, 'wb') as file:
+        file.write(image)
+
+    relative_image_path = os.path.relpath(image_path, start=os.path.dirname(recipe_path))
+    token.attrSet('src', relative_image_path)
+
+    return True
+
+def transform_images(tokens: List[Token], recipe_path: str) -> bool:
     image_found = False
 
-    for node, entering in NodeWalker(ast):
-        if entering and node.t == "image":
-            url = URL(node.destination)
-            if url.is_absolute():
-                image_found = True
-                with urlopen_user_agent(str(url)) as resp:
-                    image = resp.read()
+    for token in tokens:
+        if token.children:
+            image_found |= transform_images(token.children, recipe_path)
 
-                image_path = os.path.join(root_path, 'images',  url.name)
-                with open(image_path, 'wb') as file:
-                    file.write(image)
+        if token.type == "image":
+            image_found |= transform_image(token, recipe_path)
+            
+    return image_found
 
-                relative_image_path = os.path.relpath(image_path, start=os.path.dirname(recipe_path))
-                node.destination = relative_image_path
+def download_recipe_images(recipe_path):
+    with open(recipe_path, 'r', encoding='UTF-8') as file:
+        tokens = markdown_it.parse(file.read())
+
+    image_found = transform_images(tokens, recipe_path)
 
     if image_found:
         print(f"[Images] Processed {recipe_path}", file=sys.stderr)
-        transformed_source = commonmark_renderer.render(ast)
+        options = {}
+        env = {}
+        transformed_source = renderer.render(tokens, options, env)
         recipe = recipe_parser.parse(transformed_source)
         with open(recipe_path, 'w', encoding='UTF-8') as file:
             file.write(recipe_serializer.serialize(recipe))
